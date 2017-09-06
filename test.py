dc9261c9655e0dabc856d2e4f9914f47241bc74a
Kong Weiyi <kongweiyi@baicgroup.com.cn>
2017-09-05 19:33:06 -0700

    function

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-sum = 0
-n = 99
-while n > 0:
-    sum = sum + n
-    n = n - 2
-print(sum)
+import math
 
+def move(x,y,step,angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y + step * math.sin(angle)
+    return nx,ny
+
+def Calc(*num):
+    sum = 0
+    for n in num:
+        sum = sum + n * n
+    return sum
+def person(name,age,**kw):
+    print('name',name,'age',age,'other',kw)
+    return
+S = Calc(1,2,3,4,5,6)
+print(S)
